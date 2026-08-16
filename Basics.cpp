dbddd16e7a96372f959f9aba1b5097ac20c35369
millixs <dxnut2359@gmail.com>
2026-08-16 21:40:32 +0530

    Added comments (instructions)

--- Basics.cpp
+++ Basics.cpp
@@ -1,3 +1,7 @@
+# Use Ctrl+Shif+B to run the code through the terminal
+# Input the values in input.txt
+# Output will be displayed in output.txt
+
 #include <bits/stdc++.h>
 using namespace std;
 int main (){
